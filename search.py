3d8915d99dfea2caec53e52290bdc2fab8e3bf34
2wnsqo <2wnsqo@naver.com>
2025-05-27 17:52:48 +0900

    1753

---
+++ search.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+import meilisearch
+
+load_dotenv()
+
+client = meilisearch.Client(os.getenv("MEILI_SEARCH_URL"), os.getenv("MEILI_SEARCH_KEY"))
+
+def stock_search(query):
+    return client.index("documents").search(query)
