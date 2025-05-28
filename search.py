02e8814184239ddb4a906cf228d4f535e22b6bf7
2wnsqo <2wnsqo@naver.com>
2025-05-28 09:21:46 +0900

    last

--- search.py
+++ search.py
@@ -1,10 +1,13 @@
+from http import client
 from dotenv import load_dotenv
 import os
 import meilisearch
 
 load_dotenv()
 
-client = meilisearch.Client(os.getenv("MEILI_SEARCH_URL"), os.getenv("MEILI_SEARCH_KEY"))
-
+client = meilisearch.Client(
+    os.getenv("MEILI_SEARCH_URL"), 
+    os.getenv("MEILI_SEARCH_KEY")
+    )
 def stock_search(query):
-    return client.index("documents").search(query)
+    return client.index("nasdaq").search(query)
